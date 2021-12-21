956a5beb53f7510859742742206e617f6afc083c
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-21 14:17:34 +0400

    fix iptables

--- install/00-iptables.sh
+++ install/00-iptables.sh
@@ -1,11 +1,7 @@
 set -e -v
 ## save existing rules
 sudo iptables-save > ~/iptables-rules
-## modify rules, remove drop and reject lines
-grep -v "DROP" iptables-rules > tmpfile && mv tmpfile iptables-rules-mod
-grep -v "REJECT" iptables-rules-mod > tmpfile && mv tmpfile iptables-rules-mod
-## apply the modifications
-sudo iptables-restore < ~/iptables-rules-mod
+sudo iptables-save|grep -v DROP| grep -v REJECT| sudo iptables-restore
 ## check
 sudo iptables -L
 ## save the changes
