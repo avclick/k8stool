9726e29732dc8a8cd2187cb67024e819a4d38de0
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-21 14:36:09 +0400

    add scripts

--- install/40-controlplane.sh
+++ install/40-controlplane.sh
@@ -4,3 +4,7 @@ echo $CERTKEY
 ## save your CERTKEY for future use
 ## replace the addresses with your own
 sudo kubeadm init --apiserver-cert-extra-sans=pod1.avclick.ru,130.61.21.115 --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=pod1.avclick.ru --upload-certs --certificate-key=$CERTKEY
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
