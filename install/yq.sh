2c4c20f00d0c3f0e4e88fcbb4eda27b0b989c6e4
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-22 12:01:16 +0400

    fix

--- install/yq.sh
+++ install/yq.sh
@@ -1,4 +1,3 @@
-#wget -nc -O yq https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_amd64
-wget -nc -O /tmp/yq https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_arm64
+curl -L -o /tmp/yq https://github.com/mikefarah/yq/releases/download/v4.16.2/yq_linux_arm64
 chmod +x /tmp/yq
 sudo mv /tmp/yq /usr/local/bin
