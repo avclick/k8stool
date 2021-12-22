2d7af0783643dcbf7b5146352082f8edc9bd213a
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-22 14:44:51 +0400

    update metallb

--- install/60-metallb.sh
+++ install/60-metallb.sh
@@ -1,6 +1,6 @@
 set -e -v
 ## metallb
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
 kubectl apply -f layer2metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
