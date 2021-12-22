6a82762851e047be3555e4d20e89fef565501f17
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-22 14:51:25 +0400

    certmanager fx

--- install/81-certmanager.sh
+++ install/81-certmanager.sh
@@ -1,2 +1,12 @@
-kubectl create ns cert-manager
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
+set -e -v
+#kubectl create ns cert-manager
+#kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
+
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.6.1 \
+  --set installCRDs=true
