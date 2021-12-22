4a3c08d9aee78d2e5299f6909cc5828cedc1f476
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-22 12:31:41 +0400

    fix cermanager version

--- install/81-certmanager.sh
+++ install/81-certmanager.sh
@@ -1,2 +1,2 @@
 kubectl create ns cert-manager
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.6.1/cert-manager.yaml
