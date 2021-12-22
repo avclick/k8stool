475b83143c0ce22440fc95f9c9c312b967ef9c3b
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-22 12:19:03 +0400

    fix net

--- install/ngingress-metal-fix.sh
+++ install/ngingress-metal-fix.sh
@@ -4,5 +4,5 @@ yq e -i '
 .controller.hostPort.enabled = true |
 .controller.kind = "DaemonSet" |
 .controller.service.externalIPs += strenv(POD_PUBLIC_IP) |
-.controller.service.loadBalancerSourceRanges += strenv(POD_PUBLIC_IP)
+.controller.service.loadBalancerSourceRanges += strenv(POD_PUBLIC_IP_NET)
 ' $1
