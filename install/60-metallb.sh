1a0620b0b292db44247a4488f661819eee48c0c2
Vjacheslav Borisov <slavb@ilb.ru>
2021-12-23 15:41:50 +0400

    fix ip

--- install/60-metallb.sh
+++ install/60-metallb.sh
@@ -1,6 +1,9 @@
 set -e -v
+source .config.sh
 ## metallb
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
-kubectl apply -f layer2metallb.yaml
+cp layer2metallb.yaml /tmp/layer2metallb.yaml
+sed -i "s/\$POD_PUBLIC_IP_NET/$POD_PUBLIC_IP_NET/" /tmp/layer2metallb.yaml
+kubectl apply -f /tmp/layer2metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
