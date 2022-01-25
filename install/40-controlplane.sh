b498b2c788159e7550f50f848ec7c279461c1c7e
Vjacheslav Borisov <slavb@ilb.ru>
2022-01-25 15:22:49 +0400

    comment pod-network-cidr

--- install/40-controlplane.sh
+++ install/40-controlplane.sh
@@ -4,7 +4,7 @@ CERTKEY=$(kubeadm certs certificate-key)
 echo $CERTKEY
 ## save your CERTKEY for future use
 ## replace the addresses with your own
-sudo kubeadm init --apiserver-cert-extra-sans=$POD_HOSTNAME,$POD_PUBLIC_IP --pod-network-cidr=10.32.0.0/12 --control-plane-endpoint=$POD_HOSTNAME --upload-certs --certificate-key=$CERTKEY
+sudo kubeadm init --apiserver-cert-extra-sans=$POD_HOSTNAME,$POD_PUBLIC_IP --control-plane-endpoint=$POD_HOSTNAME --upload-certs --certificate-key=$CERTKEY #--pod-network-cidr=10.32.0.0/12
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
