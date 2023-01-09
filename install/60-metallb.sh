89f9670b235a5ab34a8b380f0abaf01cc64eb2ef
Vjacheslav Borisov <slavb@iconicompany.com>
2023-01-09 15:46:10 +0300

    metallb using helm

--- install/60-metallb.sh
+++
@@ -1,9 +0,0 @@
-set -e -v
-source .config.sh
-## metallb
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.12.1/manifests/metallb.yaml
-cp layer2metallb.yaml /tmp/layer2metallb.yaml
-sed -i "s^\$POD_PUBLIC_IP_NET^$POD_PUBLIC_IP_NET2^" /tmp/layer2metallb.yaml
-kubectl apply -f /tmp/layer2metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
---
+++ install/70-metallb.sh
@@ -0,0 +1,4 @@
+set -e -v
+helm repo add metallb https://metallb.github.io/metallb
+helm install metallb metallb/metallb
+
