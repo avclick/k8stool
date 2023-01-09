1bd4d1e54f9a165eb2d8035977f7972c2eeb5918
Vjacheslav Borisov <slavb@iconicompany.com>
2023-01-09 16:48:26 +0400

    imgress

---
+++ install/80-ingressnginx-custom.sh
@@ -0,0 +1,10 @@
+set -e -v
+kubectl create ns ingress-nginx
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm show values ingress-nginx/ingress-nginx > /tmp/ngingress-metal-custom.yaml
+#./ngingress-metal-hostport.sh /tmp/ngingress-metal-custom.yaml
+./ngingress-metal-hostnetwork.sh /tmp/ngingress-metal-custom.yaml
+
+helm install helm-ngingress ingress-nginx/ingress-nginx -n ingress-nginx --values /tmp/ngingress-metal-custom.yaml
