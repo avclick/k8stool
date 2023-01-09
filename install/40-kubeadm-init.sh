4937e52fa8d7441d2ce84cae4e24edc5bf47894c
Vjacheslav Borisov <slavb@ilb.ru>
2023-01-09 14:36:52 +0400

    updates

--- install/40-kubeadm-init.sh
+++ install/40-kubeadm-init.sh
@@ -6,4 +6,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # for example for a single-machine Kubernetes cluster for development
-kubectl taint nodes --all node-role.kubernetes.io/master-
+#kubectl taint nodes --all node-role.kubernetes.io/master-
